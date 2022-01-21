689fabd31fb6986c3262d0342a4967a64c5fdaae
Muhammad Irfan Fadilah <mirfanfad@gmail.com>
2022-01-21 20:16:59 +0700

    Commit initial state

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCu9vp-v5BwZFXS28BFY94-2WJlYrRpx_M",
+    authDomain: "signal-clone-3c377.firebaseapp.com",
+    projectId: "signal-clone-3c377",
+    storageBucket: "signal-clone-3c377.appspot.com",
+    messagingSenderId: "642293645129",
+    appId: "1:642293645129:web:09c08388bcdf61bc23a498"
+  };
